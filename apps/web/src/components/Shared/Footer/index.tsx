f79c52490d40d52c41442dc574bcf7328732b65b
mario <mario.siqueira@ripio.com>
2023-03-28 12:44:47 -0300

    font to lato, simplified footer

--- apps/web/src/components/Shared/Footer/index.tsx
+++ apps/web/src/components/Shared/Footer/index.tsx
@@ -15,32 +15,8 @@ const Footer: FC = () => {
         <span className="lt-text-gray-500 font-bold">
           &copy; {new Date().getFullYear()} {APP_NAME}
         </span>
-        <Link href="/terms">
-          <Trans>Terms</Trans>
-        </Link>
-        <Link href="/privacy">
-          <Trans>Privacy</Trans>
-        </Link>
-        <a href="https://lenster.xyz/discord" target="_blank" rel="noreferrer noopener">
-          <Trans>Discord</Trans>
-        </a>
-        <a href="https://lenster.xyz/donate" target="_blank" rel="noreferrer noopener">
-          <Trans>Donate</Trans>
-        </a>
-        <a href="https://status.lenster.xyz" target="_blank" rel="noreferrer noopener">
-          <Trans>Status</Trans>
-        </a>
-        <a href="https://feedback.lenster.xyz" target="_blank" rel="noreferrer noopener">
-          <Trans>Feedback</Trans>
-        </a>
-        <Link href="/thanks">
-          <Trans>Thanks</Trans>
-        </Link>
-        <a href="https://github.com/lensterxyz/lenster" target="_blank" rel="noreferrer noopener">
+        <a href="https://github.com/ripio/social" target="_blank" rel="noreferrer noopener">
           <Trans>GitHub</Trans>
-        </a>
-        <a href="https://translate.lenster.xyz" target="_blank" rel="noreferrer noopener">
-          <Trans>Translate</Trans>
         </a>
       </div>
       <div className="mt-2 flex space-x-4">
